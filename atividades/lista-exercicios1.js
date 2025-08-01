b69c779a183d7671d358e85cbc3360ef8c805408
VITORIA EMILLY DE BRITO NOGUEIRA <20231041110015@ifrn.local>
2025-08-01 12:58:40 -0300

    atividade incompleta

--- atividades/lista-exercicios1.js
+++ atividades/lista-exercicios1.js
@@ -1,4 +1,23 @@
 function Questão1(){
     console.log("Eu já sei professor!")
 }
-Qustão1()
+Questão1()
+function Questão2(){
+    let nome = 'Emilly'
+    console.log("olá", nome)
+}
+Questão2()
+function Questão3(){
+    let numero1 = 10
+    let numero2 = 8
+
+    let soma = numero1 + numero2
+    let subtracao = numero1 - numero2
+    let multiplicacao = numero1 * numero2
+    let divisao = numero1 / numero2
+    let restoSoma = numero1 % numero2
+
+    console.log(soma, subtracao, multiplicacao, divisao, restoSoma)
+//continuar
+}
+Questão3()
